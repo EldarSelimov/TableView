5726269c121ae94324eefc6031fcf3c89672d32d
Edar Selimov <serselimov@gmail.com>
2020-08-02 21:56:39 +0300

    initial commit

---
+++ TableViewandImageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TableViewandImageView
+//
+//  Created by Eldar on 23.07.2020.
+//  Copyright © 2020 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
